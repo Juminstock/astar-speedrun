b64178170b810ab77577b0de69143d8543cb3454
Juminstock <juminstock@gmail.com>
2023-03-03 09:18:54 -0500

    Using functions and maps

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,46 @@
 use regex::Regex;
-use std::io::stdin;
+
+// Math operations
+fn math_operation(expression: Regex, mut operation: String, operator: &str) -> String {
+    loop {
+        let caps = expression.captures(operation.as_str());
+        if caps.is_none() {
+            break;
+        }
+        let caps = caps.unwrap();
+        let caps_expression = caps.get(0).unwrap().as_str();
+        let caps_num1 = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
+        let caps_num2 = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
+        let result = match operator {
+            "+" => caps_num1 + caps_num2,
+            "-" => caps_num1 - caps_num2,
+            "*" => caps_num1 * caps_num2,
+            "/" => caps_num1 / caps_num2,
+            _ => 0,
+        };
+        operation = operation.replace(caps_expression, &result.to_string());
+    }
+    operation
+}
 
 fn main() {
-    // We use regular expressions
-    let re_multiplication = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
-    let re_division = Regex::new(r"(\d+)\s?/\s?(\d+)").unwrap();
-    let re_addition = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
-    let re_subtraction = Regex::new(r"(\d+)\s?\-\s?(\d+)").unwrap();
+    // Regex
+    let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+    let re_sub = Regex::new(r"(\d+)\s?\-\s?(\d+)").unwrap();
+    let re_mul = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+    let re_div = Regex::new(r"(\d+)\s?/\s?(\d+)").unwrap();
 
-    // We get user data
-    println!("Please, introduce your expression to calculate: ");
-    let mut expression: String = String::new();
-    stdin().read_line(&mut expression).unwrap();
+    // User input
+    println!("Enter an expression to evaluate:");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
 
-    // Multiplication
-    loop {
-        // We use user data and generate a result
-        let caps = re_multiplication.captures(&expression);
+    input = math_operation(re_mul, input.clone(), "*");
+    input = math_operation(re_div, input.clone(), "/");
+    input = math_operation(re_add, input.clone(), "+");
+    input = math_operation(re_sub, input.clone(), "-");
 
-        if caps.is_none() {
-            break;
-        }
-        let caps = caps.unwrap();
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-        let multiplication: i32 = left_value * right_value;
-        expression = expression.replace(cap_expression, &multiplication.to_string());
+    // Show results
 
-        /* For generate more details about its output we can use:
-        println!("{:?} left: {}, right: {}.", caps, left_value, right_value); */
-    };
-
-    // Division
-    loop {
-        // We use user data and generate a result
-        let caps = re_division.captures(&expression);
-
-        if caps.is_none() {
-            break;
-        }
-        let caps = caps.unwrap();
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-        let division: i32 = left_value / right_value;
-        expression = expression.replace(cap_expression, &division.to_string());
-
-        /* For generate more details about its output we can use:
-        println!("{:?} left: {}, right: {}.", caps, left_value, right_value); */
-    };
-
-    // Addition
-    loop {
-        // We use user data and generate a result
-        let caps = re_addition.captures(&expression);
-
-        if caps.is_none() {
-            break;
-        }
-        let caps = caps.unwrap();
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-        let addition: i32 = left_value + right_value;
-        expression = expression.replace(cap_expression, &addition.to_string());
-
-        /* For generate more details about its output we can use:
-        println!("{:?} left: {}, right: {}.", caps, left_value, right_value); */
-    };
-
-    // Subtraction
-    loop {
-        // We use user data and generate a result
-        let caps = re_subtraction.captures(&expression);
-
-        if caps.is_none() {
-            break;
-        }
-        let caps = caps.unwrap();
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-        let subtraction: i32 = left_value - right_value;
-        expression = expression.replace(cap_expression, &subtraction.to_string());
-
-        /* For generate more details about its output we can use:
-        println!("{:?} left: {}, right: {}.", caps, left_value, right_value); */
-    };
-
-    // We show our result
-    println!("Your result is equal to: {}", expression);
-
+    println!("Result: {}", input);
 }
