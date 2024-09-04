7353d3d4635344ee15de37db576f1dfb459ab831
Carlos Rodríguez <juminstock@gmail.com>
2024-09-04 11:09:20 -0600

    update: add public folder

--- soneium/mortal-0/lesson-3/hardhat.config.ts
+++ soneium/mortal-0/lesson-3/hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  defaultNetwork: "minato",
+  networks: {
+    minato: {
+      url: "https://rpc.minato.soneium.org/",
+      chainId: 1946,
+      accounts: ["5424a115d4a6eb41a30cdddb635decdd2cfee1b813b45870910aa2bc5dd18e41"]
+    }
+  }
 };
 
 export default config;
