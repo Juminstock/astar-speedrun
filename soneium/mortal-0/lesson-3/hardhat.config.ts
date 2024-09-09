4b680e356eb846df862cb436e2f2defabf6c017e
Carlos Rodríguez <juminstock@gmail.com>
2024-09-09 17:14:01 -0600

    update: add setting section on readme

--- soneium/mortal-0/lesson-3/hardhat.config.ts
+++ soneium/mortal-0/lesson-3/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
@@ -10,7 +11,25 @@ const config: HardhatUserConfig = {
       chainId: 1946,
       accounts: ["5424a115d4a6eb41a30cdddb635decdd2cfee1b813b45870910aa2bc5dd18e41"]
     }
+  },
+  etherscan: {
+    apiKey: {
+      'minato': 'empty'
+    },
+    customChains: [
+      {
+        network: "minato",
+        chainId: 1946,
+        urls: {
+          apiURL: "https://explorer-testnet.soneium.org/api",
+          browserURL: "https://explorer-testnet.soneium.org"
+        }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: false
   }
-};
+}
 
 export default config;
