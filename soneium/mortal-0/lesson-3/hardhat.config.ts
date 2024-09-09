97fabf2e387223264766d811c13cb8d4aec15627
Carlos Rodríguez <juminstock@gmail.com>
2024-09-09 17:54:48 -0600

    remove: changes on other lesson

--- soneium/mortal-0/lesson-3/hardhat.config.ts
+++ soneium/mortal-0/lesson-3/hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-verify"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
@@ -9,26 +8,8 @@ const config: HardhatUserConfig = {
     minato: {
       url: "https://rpc.minato.soneium.org/",
       chainId: 1946,
-      accounts: ["5424a115d4a6eb41a30cdddb635decdd2cfee1b813b45870910aa2bc5dd18e41"]
+      accounts: ["your-private-key-here"]
     }
-  },
-  etherscan: {
-    apiKey: {
-      'minato': 'empty'
-    },
-    customChains: [
-      {
-        network: "minato",
-        chainId: 1946,
-        urls: {
-          apiURL: "https://explorer-testnet.soneium.org/api",
-          browserURL: "https://explorer-testnet.soneium.org"
-        }
-      }
-    ]
-  },
-  sourcify: {
-    enabled: false
   }
 }
 
